ab26583d0cb1b652ac7f167274550e6bc2b04ac9
Michael <msimard8@gmail.com>
2019-05-02 20:59:37 -0400

    Initial Commit

---
+++ Stalker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stalker
+//
+//  Created by Michael Simard on 5/2/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
