14ed3875667b95b5dee327275f08911800299d9a
Michael <msimard8@gmail.com>
2019-05-04 15:04:12 -0400

    intial work on StalkerNetworkService, has a function that grabs news articles, no error handling yet

--- Stalker/StalkerNetworkService.swift
+++ Stalker/StalkerNetworkService.swift
@@ -9,24 +9,44 @@
 import Foundation
 import AFNetworking
 
-class NewsNetworkService: NSObject {
+class StalkerNetworkService: NSObject {
 
-    let newsAPIURL = "https://newsapi.org/v2/everything"
+    let newsAPIURL = "https://newsapi.org"
     let newsAPIKey = "50600433afee4b1db1c36a8fe9745da4"
 
-    let manager = AFHTTPSessionManager()
-    func fetchNews(subject:String){
+    let manager = AFHTTPSessionManager(baseURL: URL(string: "https://newsapi.org"))
+   
+    
+    func fetchNews(subject:String, page:Int, completion:@escaping ((_ articles:[NewsArticle])->())){
         
         let parameters = ["q":subject,
                           "apiKey":newsAPIKey,
-                          "":"" ]
+                          "page":"\(page)"
+                           ]
         
-        manager.get(newsAPIURL, parameters: parameters, progress: { (progress) in
+        manager.get("/v2/everything", parameters: parameters, progress: { (progress) in
             
         }, success: { (sessionDataTask, response) in
+             guard let json = response as? [String:Any] else {
+                return
+            }
+            
+            guard let articles = json["articles"] as? [[String:Any]] else {
+                return
+            }
+            
+            var newsArticles:[NewsArticle] = []
+            
+            for articleJSON in articles{
+               newsArticles.append(NewsArticle(title: articleJSON["title"] as? String))
+            }
+            
+            completion(newsArticles)
+            print (newsArticles)
             
         }) { (sessionDataTask, error) in
         
+            print (error)
         }
     }
     
