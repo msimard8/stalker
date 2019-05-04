1473c5a552067ea0131e14f96b8036f3dfa74e21
Michael <msimard8@gmail.com>
2019-05-04 19:14:01 -0400

    use inserts instead of loading entire table after new data comes in, much much smoother

--- Stalker/ArticleListViewController.swift
+++ Stalker/ArticleListViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ArticleListViewController: UIViewController {
     
     let maxArticleCount = 100 //(This is set by newsapi.org for dev accounts)
+    let searchSubject = "The Night King"
     
+    let stalkerNetworkService = StalkerNetworkService()
     @IBOutlet var tableView: UITableView!
     var articles:[NewsArticle] = []
     var lastPage = 0
@@ -19,22 +21,47 @@ class ArticleListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.estimatedRowHeight = 110
         self.reload(page:lastPage + 1)
         let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(reload), for: UIControl.Event.valueChanged)
+        //  refreshControl.addTarget(self, action: #selector(reload), for: UIControl.Event.valueChanged)
         self.tableView.refreshControl = refreshControl
         self.tableView.register(UINib(nibName: "ArticleListLoadMoreTableViewCell", bundle: nil), forCellReuseIdentifier: "load-more-cell")
         self.tableView.register(UINib(nibName: "ArticleListTableViewCell", bundle: nil), forCellReuseIdentifier: "article-cell")
+        self.title = searchSubject
     }
     
     @objc func reload(page:Int){
         if loading == false { //prevents duplicate loads if the user is on a slow network and scrolls up and down veyr fast
             loading = true
-            StalkerNetworkService().fetchNews(subject: "Night King", page:page) { (articles) in
+            stalkerNetworkService.fetchNews(subject: searchSubject, page:page) { (articles) in
                 DispatchQueue.main.async {
                     self.tableView.refreshControl?.endRefreshing()
-                    self.articles = self.articles + articles
-                    self.tableView.reloadData()
+                    let previousArticlesCount = self.articles.count
+                     self.articles = self.articles + articles
+                  
+                    //only reload new articles for smoothness
+                    
+                    var newIndexPaths:[IndexPath] = []
+                    for i in previousArticlesCount...previousArticlesCount + articles.count-1 {
+                        newIndexPaths.append(IndexPath(row: i, section: 0))
+                    }
+                    //the load more cell if we are under the max articles
+                    
+                    if self.articles.count < self.maxArticleCount {
+                        if (self.tableView.numberOfRows(inSection: 0) == previousArticlesCount){//only add load more cell if its not there before
+                            newIndexPaths.append(IndexPath(row:self.articles.count, section: 0))
+                        }
+                    }
+                    else if self.articles.count == self.maxArticleCount{
+                        newIndexPaths.removeLast() //no more articles, no need to show the load more cell
+                    }
+                    
+                  
+                    self.tableView.insertRows(at: newIndexPaths, with: .fade)
+ 
+                    
+                    
                     self.lastPage += 1;
                     self.loading = false
                 }
@@ -79,32 +106,58 @@ extension ArticleListViewController: UITableViewDataSource {
             return cell
         }
         else {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "article-cell") else{
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "article-cell") as? ArticleListTableViewCell else{
                 return UITableViewCell()
             }
-            cell.textLabel?.text = articles[indexPath.row].title ?? "No title"
+            let article =  articles[indexPath.row]
+            cell.newsArticle = article
             return cell
-            
         }
     }
     
-    
+    private func downloadImage( urlString:String, indexPath:IndexPath){
+        stalkerNetworkService.fetchThumbnailImage(urlString: urlString) { (image, error) in
+            DispatchQueue.main.async {
+                if self.tableView.indexPathsForVisibleRows?.contains(indexPath) ?? false {
+                    if let thumbnailImage = image {
+                        (self.tableView.cellForRow(at: indexPath) as? ArticleListTableViewCell)?.thumbnailImageView.image = thumbnailImage
+                        (self.tableView.cellForRow(at: indexPath) as? ArticleListTableViewCell)?.thumbnailImageView.backgroundColor = .clear
+                    }
+                    else {
+                        (self.tableView.cellForRow(at: indexPath) as? ArticleListTableViewCell)?.thumbnailImageView.image = nil
+                        (self.tableView.cellForRow(at: indexPath) as? ArticleListTableViewCell)?.backgroundColor = .red
+                        
+                    }
+                }
+            }
+            
+        }
+    }
 }
 
 extension ArticleListViewController: UITableViewDelegate {
     
-    //    func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
-    //        StalkerNetworkService().fetchNews(subject: "Night King") { (articles) in
-    //            DispatchQueue.main.async {
-    //                self.articles = self.articles ?? [] + articles
-    //                self.tableView.reloadData()
-    //            }
-    //        }
-    //    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if let articleCell = cell as? ArticleListTableViewCell {
+            
+            if let urlImage = articleCell.newsArticle?.urlToImage {
+                articleCell.thumbnailImageView.backgroundColor = .lightGray
+                downloadImage(urlString: urlImage, indexPath: indexPath)
+            }
+        
+            
+        }
         if let _ = cell as? ArticleListLoadMoreTableViewCell {
-            reload(page: lastPage + 1)
+
+            DispatchQueue.main.asyncAfter(deadline: .now() ) {
+                self.reload(page: self.lastPage + 1)
+
+            }
         }
     }
 }
