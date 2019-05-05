5a5de7bdf4d41a2a2ebfcac622dc749302127ee6
Michael <msimard8@gmail.com>
2019-05-05 14:51:25 -0400

    add properties to headerview, background image, and title (currently hidden)

--- Stalker/ArticleListHeaderView.swift
+++ Stalker/ArticleListHeaderView.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ArticleContentHeaderView: UITableViewHeaderFooterView {
+class ArticleListHeaderView: UITableViewHeaderFooterView {
+    static let identifier = "ArticleListHeaderView"
 
+    override var contentView: UIView {
+        return self.subviews[0]
+    }
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    //  @IBOutlet weak var imageView: UIImageView!
+  //  weak var titleLabel: UILabel!
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
