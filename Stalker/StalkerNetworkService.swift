f03561c82dfb932c2433db0b26ed3368a1e66202
Michael <msimard8@gmail.com>
2019-05-04 23:47:11 -0400

    add a function that fetches a thumbnail image

--- Stalker/StalkerNetworkService.swift
+++ Stalker/StalkerNetworkService.swift
@@ -10,24 +10,38 @@ import Foundation
 import AFNetworking
 
 class StalkerNetworkService: NSObject {
-
+    
     let newsAPIURL = "https://newsapi.org"
     let newsAPIKey = "50600433afee4b1db1c36a8fe9745da4"
+    
 
     let manager = AFHTTPSessionManager(baseURL: URL(string: "https://newsapi.org"))
-   
+    let imageDownloader = AFImageDownloader()
+    
+    
+    func fetchThumbnailImage(urlString:String, completion:@escaping ((_ image:UIImage?, _ error:Error?)->())){
+        
+        guard let imageURL = URL(string: urlString)else {
+            return
+        }
+        imageDownloader.downloadImage(for: URLRequest(url: imageURL), success: { (request, response, image) in
+            completion(image, nil)
+        }) { (request, response, error) in
+            completion(nil, error)
+        }
+    }
     
     func fetchNews(subject:String, page:Int, completion:@escaping ((_ articles:[NewsArticle])->())){
         
         let parameters = ["q":subject,
                           "apiKey":newsAPIKey,
                           "page":"\(page)"
-                           ]
+        ]
         
         manager.get("/v2/everything", parameters: parameters, progress: { (progress) in
             
         }, success: { (sessionDataTask, response) in
-             guard let json = response as? [String:Any] else {
+            guard let json = response as? [String:Any] else {
                 return
             }
             
@@ -38,14 +52,18 @@ class StalkerNetworkService: NSObject {
             var newsArticles:[NewsArticle] = []
             
             for articleJSON in articles{
-               newsArticles.append(NewsArticle(title: articleJSON["title"] as? String))
+                newsArticles.append(NewsArticle(
+                    title: articleJSON["title"] as? String,
+                    urlToImage: articleJSON["urlToImage"] as? String,
+                    source: (articleJSON["source"] as? [String:Any])?["name"] as? String,
+                    publishedAt: articleJSON["publishedAt"] as? String
+
+                    ))
             }
             
             completion(newsArticles)
-            print (newsArticles)
             
         }) { (sessionDataTask, error) in
-        
             print (error)
         }
     }
