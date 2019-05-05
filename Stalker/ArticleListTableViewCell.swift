0db7a3153f897694403b1d2cc55d030137152b5a
Michael <msimard8@gmail.com>
2019-05-04 23:46:22 -0400

    Add newsArticle property, update uI accordingly in the didSet

--- Stalker/ArticleListTableViewCell.swift
+++ Stalker/ArticleListTableViewCell.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ArticleListTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var sourceLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var newsArticle:NewsArticle? {
+        didSet{
+            self.backgroundColor = .white
+            thumbnailImageView.image = nil
+            thumbnailImageView.backgroundColor = newsArticle?.urlToImage == nil ? .clear : .lightGray
+            titleLabel.text = newsArticle?.title ?? "No title"
+            self.sourceLabel.text = newsArticle?.source ?? ""
+            self.dateLabel.text = UTCToLocal(date:   newsArticle?.publishedAt ?? "")
+            
+
+        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -17,8 +34,20 @@ class ArticleListTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
     
+    
+    func UTCToLocal(date:String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        if let formmattedDate = dateFormatter.date(from: date){
+            print (formmattedDate)
+            let displayFormatter = DateFormatter()
+            displayFormatter.locale = Locale(identifier: "en_US")
+            displayFormatter.dateFormat = "MMM d, YYYY, h:mm a"
+            return displayFormatter.string(from: formmattedDate)
+        }
+        return ""
+    }
 }
