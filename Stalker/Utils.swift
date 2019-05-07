031c1cadfb5f50c6099118138b01e38e30e0085e
Michael <msimard8@gmail.com>
2019-05-06 22:37:15 -0400

    added settings icon, select stalker view controller, settings view controller, and utils classes

---
+++ Stalker/Utils.swift
@@ -0,0 +1,13 @@
+//
+//  Utils.swift
+//  Stalker
+//
+//  Created by Michael Simard on 5/6/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import UIKit
+
+class Utils: NSObject {
+
+}
