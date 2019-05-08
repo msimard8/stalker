e3f55ef88f897e4117830d62783c30a57d8756d9
Michael <msimard8@gmail.com>
2019-05-08 15:55:10 -0400

    Added a few more tests

--- StalkerTests/StalkerTests.swift
+++ StalkerTests/StalkerTests.swift
@@ -10,17 +10,29 @@ import XCTest
 @testable import Stalker
 
 class StalkerTests: XCTestCase {
-
-    var sampleResonseData: Any?
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        //
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+
+    //CACHE
+    func testStoreAndRetreiveImage(){
+        ImageCache.shared.storeImage(key: "image", image: UIImage.emptyImage(with: CGSize(width: 20, height: 20))!)
+        XCTAssert(((ImageCache.shared.retrieveImage(key: "image")) != nil), "Image not found after storage")
+    }
+
+    func testRetrieveNonExistentImage () {
+        XCTAssert(((ImageCache.shared.retrieveImage(key: "image")) == nil), "returned image when expecting none")
+    }
+
+
+    //NETWORKING
     func testNewsResponseFormat() {
         let expectation = self.expectation(description: "network request")
         StalkerNetworkService.shared.fetchNews(subject: "bitcoin", page: 1) { (articles, response) in
@@ -52,6 +64,7 @@ class StalkerTests: XCTestCase {
 
     }
 
+    //UTILS
     func testDateFormatter() {
         //given
         let date = "2019-04-09T16:28:18Z"
@@ -59,10 +72,37 @@ class StalkerTests: XCTestCase {
         XCTAssert(Utils.formatDate(date: date) == "Apr 9, 2019, 12:28 PM")
     }
 
+    func testStalkerNameStore() {
+        UserDefaults.standard.setValue("", forKey: Utils.stalkerNameKey)
+        let stalkerName = "Jon Snow"
+        Utils.stalkerName = stalkerName
+        let userDefaultsStalkerName = UserDefaults.standard.value(forKey: Utils.stalkerNameKey) as? String ?? ""
+        UserDefaults.standard.synchronize()
+        XCTAssert(userDefaultsStalkerName == stalkerName, "Stalker name does not match what is stored")
+    }
+
+    func testStalkerNameRetrieval() {
+        UserDefaults.standard.setValue("Jon Snow", forKey: Utils.stalkerNameKey)
+        let retrievedStalkerName = Utils.stalkerName
+        UserDefaults.standard.synchronize()
+        XCTAssert(retrievedStalkerName == "Jon Snow", "Stalker name does not match what is stored")
+    }
+
     func testDateFormatterPerformance() {
         let date = "2019-04-09T16:28:18Z"
         self.measure {
-           (Utils.formatDate(date: date))
+           let _ = (Utils.formatDate(date: date))
         }
+    }
+}
+
+//source https://stackoverflow.com/questions/14594782/how-can-i-make-an-uiimage-programmatically
+//used for testing cacheing
+extension UIImage {
+    static func emptyImage(with size: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContext(size)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
     }
 }
