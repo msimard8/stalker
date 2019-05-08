50c5bb41ca07167b7f29cae322fa211828da6e2a
Michael <msimard8@gmail.com>
2019-05-08 13:41:40 -0400

    new classes for EyeView and ImageCache

---
+++ Stalker/ImageCache.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCache.swift
+//  Stalker
+//
+//  Created by Michael Simard on 5/7/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import UIKit
+
+class ImageCache: NSObject {
+
+}
