1f3642e814fb4c8739c837825e1ffd1b64050195
Michael <msimard8@gmail.com>
2019-05-08 13:49:20 -0400

    added a Utils class that will store the stalked perosn int he user defaults, also holds the time formatter

--- Stalker/Utils.swift
+++ Stalker/Utils.swift
@@ -9,5 +9,27 @@
 import UIKit
 
 class Utils: NSObject {
+    static let stalkerNameKey = "stalker-name"
 
+    static var stalkerName: String {
+        set {
+            UserDefaults.standard.setValue(newValue, forKey: stalkerNameKey)
+        }
+        get {
+            return UserDefaults.standard.value(forKey: stalkerNameKey) as? String ?? ""
+        }
+    }
+
+    static func formatDate(date: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        if let formmattedDate = dateFormatter.date(from: date) {
+            print (formmattedDate)
+            let displayFormatter = DateFormatter()
+            displayFormatter.locale = Locale(identifier: "en_US")
+            displayFormatter.dateFormat = "MMM d, YYYY, h:mm a"
+            return displayFormatter.string(from: formmattedDate)
+        }
+        return ""
+    }
 }
