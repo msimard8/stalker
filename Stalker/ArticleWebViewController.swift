37b90f30f523c16d17382edfcfa1aeeffbd50169
Michael <msimard8@gmail.com>
2019-07-13 23:01:25 -0400

    show loading indicator in web view

--- Stalker/ArticleWebViewController.swift
+++ Stalker/ArticleWebViewController.swift
@@ -12,10 +12,13 @@ import WebKit
 class ArticleWebViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var eyeView: EyeView!
+    @IBOutlet weak var loadingLabel: UILabel!
 
     var articleUrlString: String?
 
     override func viewDidLoad() {
+        webView.navigationDelegate = self
         super.viewDidLoad()
         if let urlString = articleUrlString {
             if let url =  URL(string: urlString) {
@@ -24,10 +27,14 @@ class ArticleWebViewController: UIViewController {
         }
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        eyeView.animate()
+    }
+
     override func viewWillDisappear(_ animated: Bool) {
         webView.stopLoading()
     }
-
 
     /*
      // MARK: - Navigation
@@ -39,4 +46,17 @@ class ArticleWebViewController: UIViewController {
      }
      */
 
+}
+
+extension ArticleWebViewController : WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        DispatchQueue.main.async {
+            self.eyeView.isHidden = true
+            self.loadingLabel.isHidden = true
+        }
+    }
 }
