d2ae914db874c2b17e1f13bc9dce6f8cdd47c9d4
Michael <msimard8@gmail.com>
2019-07-13 21:35:58 -0400

    added a split view controller for better presentation on ipad

---
+++ Stalker/ArticleSplitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArticleSplitViewController.swift
+//  Stalker
+//
+//  Created by Michael Simard on 7/13/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import UIKit
+
+class ArticleSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
