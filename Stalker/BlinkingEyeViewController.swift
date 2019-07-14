309dc3c7d2bd4648f8f50a0e289162796dbc22e2
Michael <msimard8@gmail.com>
2019-07-13 22:03:12 -0400

    Animated eye on appear

--- Stalker/BlinkingEyeViewController.swift
+++ Stalker/BlinkingEyeViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class BlinkingEyeViewController: UIViewController {
+    @IBOutlet weak var eyeView: EyeView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +17,10 @@ class BlinkingEyeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        eyeView.animate()
+    }
 
     /*
     // MARK: - Navigation
