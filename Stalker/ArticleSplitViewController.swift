db225aeb4ccf9192e01b499efdd395609fcab130
Michael <msimard8@gmail.com>
2019-07-13 22:02:57 -0400

    show blinking eye view controller if no article is selected

--- Stalker/ArticleSplitViewController.swift
+++ Stalker/ArticleSplitViewController.swift
@@ -15,27 +15,19 @@ class ArticleSplitViewController: UISplitViewController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 
-        let selectStalkerViewController = SelectStalkerViewController()
-        let navigationController = UINavigationController(rootViewController: selectStalkerViewController)
-
-                if Utils.stalkerName != "" {
-                    let articleListViewController = ArticleListViewController()
-                    navigationController.pushViewController(articleListViewController, animated: false)
-                    articleListViewController.view.backgroundColor = UIColor.white
-                    articleListViewController.delegate = self
-                 }
-
+        let articleListViewController = ArticleListViewController()
+        articleListViewController.view.backgroundColor = UIColor.white
+        articleListViewController.delegate = self
+        let navigationController = UINavigationController(rootViewController: articleListViewController)
         navigationController.navigationBar.barTintColor = .black
 
         UIBarButtonItem.appearance().tintColor = UIColor.white
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        self.viewControllers = [navigationController,articleContentViewController]
+        self.viewControllers = [navigationController,BlinkingEyeViewController()]
 
         self.preferredDisplayMode = .allVisible
         delegate = self
     }
-
-
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -52,14 +44,14 @@ class ArticleSplitViewController: UISplitViewController {
     }
 
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 
 }
 
@@ -68,18 +60,18 @@ extension ArticleSplitViewController :UISplitViewControllerDelegate {
     func splitViewController(_ svc: UISplitViewController, willChangeTo displayMode: UISplitViewController.DisplayMode) {
 
     }
-//    func splitViewController(_ splitViewController: UISplitViewController, showDetail vc: UIViewController, sender: Any?) -> Bool {
-//        if let articleContentViewController = vc as? ArticleContentViewController {
-//            return articleContentViewController.newsArticle != nil
-//    }
-//        return true 
-//    }
+    //    func splitViewController(_ splitViewController: UISplitViewController, showDetail vc: UIViewController, sender: Any?) -> Bool {
+    //        if let articleContentViewController = vc as? ArticleContentViewController {
+    //            return articleContentViewController.newsArticle != nil
+    //    }
+    //        return true
+    //    }
 }
 
 extension ArticleSplitViewController : ArticleListViewControllerDelegate {
 
     func didSelectArticle(article: NewsArticle) {
-       // articleContentViewController.loadView()
+        articleContentViewController.loadView()
         articleContentViewController.newsArticle = article
         self.showDetailViewController(articleContentViewController, sender: self)
     }
