171f48a94fae3e93a969ff57de24442e89ef18f0
Michael <msimard8@gmail.com>
2019-07-13 21:40:05 -0400

    intitial setup, implemented the AritcleListViewControllerDelegate that has the didSelectArticle function thats used to change details, override functions required to show master first on compact devices

--- Stalker/ArticleSplitViewController.swift
+++ Stalker/ArticleSplitViewController.swift
@@ -10,12 +10,46 @@ import UIKit
 
 class ArticleSplitViewController: UISplitViewController {
 
+    let articleContentViewController = ArticleContentViewController()
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+        let selectStalkerViewController = SelectStalkerViewController()
+        let navigationController = UINavigationController(rootViewController: selectStalkerViewController)
+
+                if Utils.stalkerName != "" {
+                    let articleListViewController = ArticleListViewController()
+                    navigationController.pushViewController(articleListViewController, animated: false)
+                    articleListViewController.view.backgroundColor = UIColor.white
+                    articleListViewController.delegate = self
+                 }
+
+        navigationController.navigationBar.barTintColor = .black
+
+        UIBarButtonItem.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        self.viewControllers = [navigationController,articleContentViewController]
+
+        self.preferredDisplayMode = .allVisible
+        delegate = self
+    }
+
+
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
+
+
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        return true
+    }
 
     /*
     // MARK: - Navigation
@@ -27,4 +61,26 @@ class ArticleSplitViewController: UISplitViewController {
     }
     */
 
+}
+
+extension ArticleSplitViewController :UISplitViewControllerDelegate {
+
+    func splitViewController(_ svc: UISplitViewController, willChangeTo displayMode: UISplitViewController.DisplayMode) {
+
+    }
+//    func splitViewController(_ splitViewController: UISplitViewController, showDetail vc: UIViewController, sender: Any?) -> Bool {
+//        if let articleContentViewController = vc as? ArticleContentViewController {
+//            return articleContentViewController.newsArticle != nil
+//    }
+//        return true 
+//    }
+}
+
+extension ArticleSplitViewController : ArticleListViewControllerDelegate {
+
+    func didSelectArticle(article: NewsArticle) {
+       // articleContentViewController.loadView()
+        articleContentViewController.newsArticle = article
+        self.showDetailViewController(articleContentViewController, sender: self)
+    }
 }
