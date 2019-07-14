aeb52756c70406b192f198c9b7b921f3581c68c4
Michael <msimard8@gmail.com>
2019-07-13 22:38:33 -0400

    load web page

--- Stalker/ArticleWebViewController.swift
+++ Stalker/ArticleWebViewController.swift
@@ -7,24 +7,36 @@
 //
 
 import UIKit
+import WebKit
 
 class ArticleWebViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+
+    var articleUrlString: String?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let urlString = articleUrlString {
+            if let url =  URL(string: urlString) {
+            webView.load(URLRequest(url: url))
+            }
+        }
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewWillDisappear(_ animated: Bool) {
+        webView.stopLoading()
     }
 
 
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 
 }
